7aeb39555097e46c34ec7f8757c57382abda4a04
Prashant Katiyar <prashantkatiyar07@gmail.com>
2017-11-29 14:32:45 +0530

    initial

--- SpringBootMongoDB-master/src/main/java/com/dineshonjava/sbmdb/SpringBootMongoDbApplication.java
+++ SpringBootMongoDB-master/src/main/java/com/dineshonjava/sbmdb/SpringBootMongoDbApplication.java
@@ -2,7 +2,9 @@ package com.dineshonjava.sbmdb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
+//(exclude = {MongoAutoConfiguration.class, MongoDataAutoConfiguration.class})
 @SpringBootApplication
 public class SpringBootMongoDbApplication {
 
